ff464f36c47fb245103d3a7a43bc7515d1edd021
Jonah Scheinerman <jonah@scheinerman.net>
2015-10-04 17:59:49 +0100

    Refactor normal distro

--- src/normal.py
+++ src/normal.py
@@ -1,10 +1,17 @@
-import scipy.stats as stats, distro, math
+import distro
 
-Normal = distro.Distribution(
-    name   = 'Normal',
-    domain = distro.Domain.Continuous,
-    cdf    = lambda x, (mu, sig) : stats.norm.cdf(x, mu, sig),
-    solver = lambda mean, var: (mean, math.sqrt(var)),
-    params = ('mu', 'sigma')
+def _solver(stats):
+    (mu, sig2) = distro.extractStats(stats, [distro.Stat.Mu, distro.Stat.Sig2])
+    return (mu, math.sqrt(sig2))
+
+normal = distro.Distribution(
+    name        = 'Normal',
+    domain      = distro.Domain.Continuous,
+    params      = ('mu', 'sigma'),
+    paramSolver = _solver,
+    cdf         = lambda x : 0, # unimplemented
+    fittingFns  = {
+        distro.Stat.Skew: lambda mu, sig : 0.0
+        distro.Stat.Kurt: lambda mu, sig : 0.0
+    }
 )
-print Normal
